ca3ff8ebee9bc530d2debe636d07c709cd259eff
Brian Muranja <brianwaweru58@gmail.com>
2025-06-17 00:33:42 +0200

    feat(deployment) test 21

--- app/src/app/backend/api/application/route.ts
+++ app/src/app/backend/api/application/route.ts
@@ -1,33 +1,33 @@
-// import { PrismaClient } from '@/src/app/backend/generated/prisma'
-// const prisma = new PrismaClient()
-// import { NextResponse } from 'next/server'
+import { PrismaClient } from '@/src/app/backend/generated/prisma'
+const prisma = new PrismaClient()
+import { NextResponse } from 'next/server'
 
-// export async function GET() {
-// // TODO: Create a application with a student_id and thesis_id
-//   const applications = await prisma.application.findMany({
-//     include: {
-//       student: {
-//         include: {
-//           user_parent: true
-//         }
-//       },
-//       thesis_proposal: true
-//     }
-//   })
+export async function GET() {
+// TODO: Create a application with a student_id and thesis_id
+  const applications = await prisma.application.findMany({
+    include: {
+      student: {
+        include: {
+          user_parent: true
+        }
+      },
+      thesis_proposal: true
+    }
+  })
 
-//   return NextResponse.json({ message: "Not implemented" })
-// }
+  return NextResponse.json(applications)
+}
 
-// export async function POST(req: Request) {
-//   const data = await req.json()
+export async function POST(req: Request) {
+  const data = await req.json()
 
-//   const application = await prisma.application.create({
-//     data: {
-//       student_id: data.student_id,
-//       thesis_id: data.thesis_id,
-//       status: 'PENDING' // or your enum/string default
-//     }
-//   })
+  const application = await prisma.application.create({
+    data: {
+      student_id: data.student_id,
+      thesis_id: data.thesis_id,
+      status: 'PENDING' // or your enum/string default
+    }
+  })
 
-//   return NextResponse.json({ message: "Not implemented" })
-// }
+  return NextResponse.json(application)
+}
